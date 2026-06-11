94c125aea01c69dc575ae1e0f4425057d14d3a66
rithikashsvp25ct-star <rithikashsvp25ct@vetias.ac.in>
2026-06-11 16:08:27 +0530

    constructor added

--- src/Day5.java
+++ src/Day5.java
@@ -1,12 +1,12 @@
 public class Day5 {
 
-    static class Car {
+/*static class Car {
         String color;
         String brand;
         int speed;
         int fuel;
 
-        public Car(String brand, String color, int fuel) {
+        Car(String brand, String color, int fuel) {
             this.brand = brand;
             this.color = color;
             this.fuel = fuel;
@@ -14,18 +14,19 @@ public class Day5 {
         }
 
         public void accelerate() {
+            System.out.println("Brand :"+brand);
+            System.out.println("Colour :"+color);
             if (fuel > 0) {
                 speed += 10;
                 fuel -= 5;
-                System.out.println(" accelerated. Speed: " + speed +
-                                   " km/h | Fuel: " + fuel + "%");
+                System.out.println("accelerated. Speed: " + speed +" km/h | Fuel: " + fuel + "%");
             } 
         }
 
         public void decelerate() {
             if (speed > 0) {
                 speed -= 10;
-                System.out.println( " slowed down. Speed: " + speed + " km/h");
+                System.out.println("slowed down. Speed: " + speed + " km/h");
             } 
         }
 
@@ -47,9 +48,65 @@ public class Day5 {
 
         myCar.showFuel();
         myCar.accelerate();
-        myCar.accelerate();
         myCar.decelerate();
         myCar.refill();
         myCar.showFuel();
+    }*/
+ static   class Student {
+    String name, dept;
+    int regNo;
+    int maths, physics, chemistry;
+    Student(String n, int r, String d) {
+        name = n;
+        regNo = r;
+        dept = d;
+    }
+    void setMaths(int m) { 
+        maths = m; 
+        }
+    void setPhysics(int p) { 
+        physics = p; 
+        }
+    void setChemistry(int c) { 
+        chemistry = c;
+        }
+
+
+    void updateMarks(int m) {
+        maths = m;
+    }
+
+    void updateMarks(int m, int p) {
+        maths = m;
+        physics = p;
+    }
+
+    void updateMarks(int m, int p, int c) {
+        maths = m;
+        physics = p;
+        chemistry = c;
+    }
+
+    void show() {
+        System.out.println(name + " " + regNo + " " + dept);
+        System.out.println(maths + " " + physics + " " + chemistry);
+        System.out.println("------------");
+    }
+}
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Arun", 101, "CSE");
+        Student s2 = new Student("Bala", 102, "ECE");
+        Student s3 = new Student("Charan", 103, "MECH");
+
+        s1.updateMarks(80, 85, 90);
+        s2.setMaths(70);
+        s2.setPhysics(75);
+        s2.setChemistry(65);
+        s3.updateMarks(88);
+
+        s1.show();
+        s2.show();
+        s3.show();
     }
 }
---
+++ src/Day5marks.java
@@ -0,0 +1,76 @@
+public class Day5marks {
+
+    static class Student {
+        String name, dept;
+        int regNo;
+        int maths, physics, chemistry;
+
+        Student(String n, int r, String d) {
+            name = n;
+            regNo = r;
+            dept = d;
+        }
+        int getMaths() {
+            return this.maths ;
+        }
+        int getPhysics() {
+           return this.physics;
+        }
+
+        int getChemistry() {
+            return this.chemistry;
+        }
+
+        void setMaths(int m) {
+            this .maths=m;
+        }
+
+        void setPhysics(int p) {
+          this.physics = p;
+        }
+
+        void setChemistry(int c) {
+            this.chemistry = c;
+        }
+        void updateMarks(int m) {
+            maths = m;
+        }
+
+        void updateMarks(int p, int c) {
+            physics = p;
+            chemistry=c;
+        }
+
+        void updateMarks(int m, int p, int c) {
+            maths = m;
+            physics = p;
+            chemistry = c;
+        }
+
+        void show() {
+            System.out.println(name + " " + regNo + " " + dept);
+            System.out.println("Maths     : " + getMaths());
+            System.out.println("Physics   : " + getPhysics());
+            System.out.println("Chemistry : " + getChemistry());
+            System.out.println("------------");
+        }
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student("Arun", 101, "CSE");
+        Student s2 = new Student("Bala", 102, "ECE");
+        Student s3 = new Student("Charan", 103, "MECH");
+        s1.updateMarks(80,89, 90);
+        s2.setMaths(70);
+        s2.setPhysics(75);
+        s2.setChemistry(65);
+        s3.updateMarks(88);
+        s3.updateMarks(90, 89);
+        s1.show();
+        s2.show();
+        s3.show();
+    System.out.println("Arun Maths Mark: " + s1.getMaths());
+    System.out.println("Bala Physics Mark: " + s2.getPhysics());
+    System.out.println("Charan Chemistry Mark: " + s3.getChemistry());
+    }
+}
+
