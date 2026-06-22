0efbfb77e109ab08f048eae69c65d386055a5d1c
misbah <misbahb25aid@vetias.ac.in>
2026-06-22 10:10:40 +0530

    "Report added"

--- src/Day13.java
+++ src/Day13.java
@@ -7,25 +7,29 @@ import java.util.List;
             Path path = Path.of("src/file","report.txt");
 
 
-            try{
+            try {
                 List<String> data = Files.readAllLines(path);
 
-                for (String line : data) {
-                    System.out.println(line);
+                for (String line : data){
+                        String[] row = line.split(",");
+                        for(int i = 0; i < row.length; i++){
+                            System.out.print(row[i]+" | ");
+                        }
+                        System.out.println();
                 }
-                List<String> newData = new ArrayList<>();
-                newData.add("Student1");
-                newData.add("Student2");
-                newData.add("Student3");
-                newData.add("Student4");
-                newData.add("Student5");
-                //Files.write(path, newData);
-                //Files.writeString(path, newData.get(4));
+                    List<String> newData = new ArrayList<>();
+                    newData.add("Student1");
+                    newData.add("Student2");
+                    newData.add("Student3");
+                    newData.add("Student4");
+                    newData.add("Student5");
+                    //Files.write(path, newData);
+                    //Files.writeString(path, newData.get(4));
 
-            } catch (IOException io){
-                System.out.println("Error reading file");
+                } catch (IOException io){
+                System.out.println("Error to reading file");
             }
-        }
+            }
 
 
 
